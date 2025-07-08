110a28ca920cd816a3911d6cba052eb2fc69ee97
Rajshree <rajshreeakm@gmail.com>
2025-07-08 14:05:56 +0530

    Valid palindrome 2 Problem

---
+++ src/_06_Strings/ValidPalindrom_2.java
@@ -0,0 +1,32 @@
+package _06_Strings;
+
+public class ValidPalindrom_2 {
+    public static boolean validPalindrome(String s) {
+        return isValid(s, 0, s.length() - 1, true);
+    }
+
+    private static boolean isValid(String s, int left, int right, boolean canDelete) {
+        while (left < right) {
+            if (s.charAt(left) == s.charAt(right)) {
+                left++;
+                right--;
+            } else {
+                if (!canDelete) {
+                    return false;
+                }
+                // Try skipping left or right character
+                return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
+            }
+        }
+        return true;
+    }
+
+    private static boolean isPalindrome(String s, int left, int right) {
+        while (left < right) {
+            if (s.charAt(left++) != s.charAt(right--)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
