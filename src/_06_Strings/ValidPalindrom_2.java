04aaabe4d84862756134c7590d0dbc57cca0f177
Rajshree <rajshreeakm@gmail.com>
2025-07-08 14:07:08 +0530

    Valid palindrome 2 Problem

--- src/_06_Strings/ValidPalindrom_2.java
+++ src/_06_Strings/ValidPalindrom_2.java
@@ -1,5 +1,12 @@
 package _06_Strings;
 
+
+/*
+Leetcode : 680. Valid Palindrome II
+
+* Given a string s, return true if the s can be palindrome after deleting at most one character from it.
+*
+* */
 public class ValidPalindrom_2 {
     public static boolean validPalindrome(String s) {
         return isValid(s, 0, s.length() - 1, true);
