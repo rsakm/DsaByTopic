6dc347dbe7b7789981ab90b093819736eed39da1
Rajshree <rajshreeakm@gmail.com>
2025-07-09 19:31:11 +0530

    Is power of 3 problem

---
+++ src/_00_MathsProblem/PowerOf3.java
@@ -0,0 +1,17 @@
+package _00_MathsProblem;
+
+public class PowerOf3 {
+    public static boolean isPowerOfThree(int n) {
+        if (n <= 0) return false;
+
+        while (n % 3 == 0) {
+            n /= 3;
+        }
+
+        return n == 1;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
