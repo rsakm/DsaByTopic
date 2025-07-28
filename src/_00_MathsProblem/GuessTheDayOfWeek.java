aa6499d624ed8f80c742184d5375ea36f7c45ee8
Rajshree <rajshreeakm@gmail.com>
2025-07-28 16:05:07 +0530

    Guess the day from given date

--- src/_00_MathsProblem/GuessTheDayOfWeek.java
+++ src/_00_MathsProblem/GuessTheDayOfWeek.java
@@ -1,5 +1,17 @@
 package _00_MathsProblem;
 
+/*
+* Leetcode: 1185. day of the week
+*
+* Given a date, return the corresponding day of the week for that date.
+
+The input is given as three integers representing the day, month and year respectively.
+
+Return the answer as one of the following values
+{"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"}.
+
+ */
+
 public class GuessTheDayOfWeek {
     public static String dayOfTheWeek(int day, int month, int year) {
         if (month == 1 || month == 2) {
@@ -12,6 +24,39 @@ public class GuessTheDayOfWeek {
         int K = year % 100;
         int J = year / 100;
 
+        /*
+        * Zeller’s Congruence
+        *
+        * Zeller’s Congruence Formula (for Gregorian calendar):
+        * h = (q + 13*(m+1)/5 + K + K/4 + J/4 + 5*J) % 7
+        *
+        * Where:
+
+q = day of the month
+
+m = month (3 = March, 4 = April, ..., 12 = December, January and February are counted as months 13 and 14 of the previous year)
+
+K = year % 100 (last two digits of the year)
+
+J = year / 100 (first two digits of the year)
+
+The result h is:
+
+0 = Saturday
+
+1 = Sunday
+
+2 = Monday
+
+3 = Tuesday
+
+4 = Wednesday
+
+5 = Thursday
+
+6 = Friday
+
+        */
         int h = (q + (13 * (m + 1)) / 5 + K + (K / 4) + (J / 4) + (5 * J)) % 7;
 
         String[] days = {"Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
