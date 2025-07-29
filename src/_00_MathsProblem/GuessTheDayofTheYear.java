217534e6eaeb13806359748ad79ed659b3bce332
Rajshree <rajshreeakm@gmail.com>
2025-07-29 16:38:22 +0530

    Day of the year

---
+++ src/_00_MathsProblem/GuessTheDayofTheYear.java
@@ -0,0 +1,37 @@
+package _00_MathsProblem;
+
+public class GuessTheDayofTheYear {
+    public static int dayOfYear(String date) {
+        int[] daysInMonth = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+        int year = Integer.parseInt(date.substring(0, 4));
+        int month = Integer.parseInt(date.substring(5, 7));
+        int day = Integer.parseInt(date.substring(8));
+
+        // Check for leap year
+        if (isLeapYear(year)) {
+            daysInMonth[1] = 29; // February becomes 29 days
+        }
+
+        int dayOfYear = 0;
+        for (int i = 0; i < month - 1; i++) {
+            dayOfYear += daysInMonth[i];
+        }
+
+        dayOfYear += day;
+
+        return dayOfYear;
+    }
+
+    private static boolean isLeapYear(int year) {
+        // Leap year if divisible by 4, but not by 100, unless divisible by 400
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(dayOfYear("2019-02-10"));
+        System.out.println(dayOfYear("2000-12-11"));
+        System.out.println(dayOfYear("2025-07-29"));
+    }
+}
