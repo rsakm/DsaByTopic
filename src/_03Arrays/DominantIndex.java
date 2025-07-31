23d841bec669a2a5fe30671bd038d987da0dc3af
Rajshree <rajshreeakm@gmail.com>
2025-07-31 20:30:17 +0530

    Largest Number At Least Twice of Others in array

--- src/_03Arrays/DominantIndex.java
+++ src/_03Arrays/DominantIndex.java
@@ -1,5 +1,16 @@
 package _03Arrays;
 
+/*
+* Leetcode: 747. Largest Number At Least Twice of Others
+*
+* You are given an integer array nums where the largest integer is unique.
+
+Determine whether the largest element in the array is at least twice as much as
+every other number in the array. If it is, return the index of the largest element,
+or return -1 otherwise.
+
+
+*/
 public class DominantIndex {
     public static int dominantIndex(int[] nums) {
         int max = -1, secondMax = -1, index = -1;
